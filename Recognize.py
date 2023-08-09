4d2ddba4da2a4c934223327e8d3e16f6efcdc9de
Akhil Sharma <akhilsharma.off@gmail.com>
2023-08-09 20:26:16 +0530

    baldev files

--- Recognize.py
+++ Recognize.py
@@ -1,29 +1,4 @@
-import pyttsx3
-import datetime
-
-engine =pyttsx3.init()
+from Speech import speak
 
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
-        engine.say("Good Morning sir! ,Back online")
-
-    elif hour>=12 and hour<18:
-        engine.say("Good Afternoon sir! ,Back online")
-
-    else:
-        engine.say("Good Evening sir! ,back online ")
-
-    speak("I am jarvis how may i help you")
-# engine = pyttsx3.init()
-	
-# engine.say("I will speak this text")
-# engine.runAndWait()
-
-if __name__ == "__main__":
-    wishMe()
+speak("Say hello, to do task..")
