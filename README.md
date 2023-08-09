7037aaf2de1884a565422ca482874696c8ac6ba0
Akhil Sharma <akhilsharma.off@gmail.com>
2023-08-09 12:56:29 +0530

    speak file

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Baldev
 
 1. pip install -r requirements.txt
+2.  Check that pyobjc is installed `(pip show pyobjc)`, if not install as pip install pyobjc. 
+3. open this file `baldev-bot/lib/python3.11/site-packages/pyttsx3/drivers/nsss.py`
+4. replace this in line 14 `self = objc.super(NSSpeechDriver, self).init()`
 3. run Recognize.py file
--- Recognize.py
+++ Recognize.py
@@ -1,49 +1,29 @@
-import speech_recognition
 import pyttsx3
 import datetime
 
-# engine =pyttsx3.init()
+engine =pyttsx3.init()
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        engine.say("Good Morning sir! ,Back online")
+
+    elif hour>=12 and hour<18:
+        engine.say("Good Afternoon sir! ,Back online")
+
+    else:
+        engine.say("Good Evening sir! ,back online ")
+
+    speak("I am jarvis how may i help you")
+# engine = pyttsx3.init()
+	
 # engine.say("I will speak this text")
 # engine.runAndWait()
-# # voice  =engine.getProperty('voices')
-# # engine.setProperty('voice',voice[0].id)
-
-# def speak(audio):
-#     engine.say(audio)
-#     engine.runAndWait()
-
-# def wishMe():
-#     hour = int(datetime.datetime.now().hour)
-#     if hour>=0 and hour<12:
-#         speak("Good Morning sir! ,Back online")
-
-#     elif hour>=12 and hour<18:
-#         speak("Good Afternoon sir! ,Back online")
-
-#     else:
-#         speak("Good Evening sir! ,back online ")
-
-#     speak("I am jarvis how may i help you")
-
-
-def TakeCommand():
-    
-    recognizer = speech_recognition.Recognizer()
-    with speech_recognition.Microphone() as mic:
-            
-        print("Listening...")
-        recognizer.pause_threshold = 1
-        try:
-            audio = recognizer.listen(mic, timeout=3)  # Set a timeout of 5 seconds for listening
-        except speech_recognition.WaitTimeoutError:
-            print("Timeout reached. No speech detected.")
-            return
-        
-    print("Recognizing...")
-    query = recognizer.recognize_google(audio, language='en-in')
-    print(f"User said: {query}\n")
-      
 
 if __name__ == "__main__":
-    #wishMe()
-    TakeCommand()
+    wishMe()
