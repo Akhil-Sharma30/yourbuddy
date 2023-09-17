526da80cada52dc91b97476e14d2e2cfd08705f7
Akhil Sharma <akhilsharma.off@gmail.com>
2023-09-17 12:42:29 +0530

    Add chatbot_choice function

--- llm_model.py
+++ llm_model.py
@@ -1,5 +1,5 @@
 
-from llama_index import VectorStoreIndex,download_loader, VectorStoreIndex, ServiceContext, StorageContext, load_index_from_storage
+from llama_index import VectorStoreIndex, download_loader, VectorStoreIndex, ServiceContext, StorageContext, load_index_from_storage
 from pathlib import Path
 from github import Github
 import os
@@ -11,18 +11,20 @@ import pypandoc
 from pathlib import Path
 from llama_index import download_loader
 
-# OPENAI key 
+# OPENAI key
 openai.api_key = os.environ.get("OPENAPI_API_KEY")
 
 """# Reading the Files for LLM Model"""
+
+
 class Llm_Training:
 
     def get_file_input():
-    # Ask the user for their choice (link or upload)
+        # Ask the user for their choice (link or upload)
         print("How would you like to provide the file?")
         print("1. Link")
         print("2. Upload")
-        
+
         choice = input("Enter your choice (1 or 2): ")
 
         # Validate the user's choice
@@ -37,9 +39,10 @@ class Llm_Training:
         else:
             # If the user chose 'upload', you can implement the file upload logic here
             # and return the uploaded file's path or content.
-            uploaded_file = input("Upload the file: ")  # Replace with your upload logic
+            # Replace with your upload logic
+            uploaded_file = input("Upload the file: ")
             return uploaded_file
-        
+
     # Converts a file to markdown format.
     def convert_to_markdown(input_file, output_file):
 
@@ -49,16 +52,17 @@ class Llm_Training:
 
         # Returns:
         # bool: True if conversion is successful, False otherwise.
-    
+
         try:
             # Convert the input file to markdown format
-            pypandoc.convert_file(input_file, 'markdown', outputfile=output_file)
+            pypandoc.convert_file(input_file, 'markdown',
+                                  outputfile=output_file)
             return True
         except Exception as e:
             print(f"Conversion error: {str(e)}")
             return False
-        
-    def chatbot_choice():
+
+    def chatbot_choice(user_input):
         # Ask the user for their choice (personal data or general chatbot)
         print("Choose an option:")
         print("1. Train on personal data")
@@ -68,18 +72,19 @@ class Llm_Training:
 
         # Validate the user's choice
         if choice not in ['1', '2']:
-            print("Invalid choice. Please enter '1' for personal data or '2' for a general chatbot.")
+            print(
+                "Invalid choice. Please enter '1' for personal data or '2' for a general chatbot.")
             return
 
         if choice == '1':
             # User selected training on personal data
-            print("Training on personal data...")  # Replace with your logic
-            # Perform training on personal data here
+            print("Training on personal data...")
+            response_data = chatbot_function(user_input)
 
         else:
             # User selected using a general chatbot
-            print("Using a general chatbot like OpenAI...")  # Replace with your logic
-            # Use a general chatbot like OpenAI here
+            print("Using a general chatbot like OpenAI...")
+            response_data = Generative_response(user_input)
 
     def chatbot_function(query):
         # Set up the OpenAI API key
@@ -108,8 +113,8 @@ class Llm_Training:
             model="gpt-3.5-turbo",
             messages=[
                 {
-                "role": "user",
-                "content": ""
+                    "role": "user",
+                    "content": ""
                 }
             ],
             temperature=1,
@@ -119,12 +124,14 @@ class Llm_Training:
             presence_penalty=0
         )
         return gpt_response.choices[0].message["content"]
-    
+
+
 # Example usage:
 if __name__ == "__main__":
     output_file = "output.md"  # Replace with your desired output file path
     input_file = Llm_Training.get_file_input()  # Replace with your input file path
     if Llm_Training.convert_to_markdown(input_file, output_file):
-        print(f"File '{input_file}' converted to markdown successfully and saved as '{output_file}'.")
+        print(
+            f"File '{input_file}' converted to markdown successfully and saved as '{output_file}'.")
     else:
         print(f"Failed to convert '{input_file}' to markdown.")
